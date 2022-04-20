dbd8a6140b8fb2d7e1ee6b0545749b53ac866688
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-20 23:26:28 +0800

    Creates a service object for fetching listings from Empire FLippers API and save it in the Listing model. Only new listings from the API are saved

---
+++ app/services/fetch_new_listings_from_empire_flippers.rb
@@ -0,0 +1,39 @@
+class FetchNewListingsFromEmpireFlippers < ApplicationService
+  def self.run
+    # Checks how many pages if limit is 20 per query
+    pages = Ef::Client.get_listings(20)[:data]["data"]["pages"]
+    # Checks all the total count of all listings in the API
+    listing_count = Ef::Client.get_listings(1)[:data]["data"]["count"]
+    # Iterates based on the number of pages
+
+    pages.times do |count|
+      start = Time.now
+      # Fetches 20 listings from page equal to the current index + 1
+      result = Ef::Client.get_listings(20, count + 1)[:data]["data"]["listings"]
+      
+      # Iterates over the result and creates new listing to the Listing model
+      result.each do |listing|
+        # Checks if there is an existing listing where listing_number is equal to listing["listing_number"]
+        unless Listing.exists?(listing_number: listing["listing_number"])
+          new_listing = Listing.new()
+
+          new_listing.listing_number = listing["listing_number"]
+          new_listing.listing_price = listing["listing_price"]
+          new_listing.summary = listing["summary"]
+          new_listing.close_date = (Time.now + 30.days).strftime("%FT%TZ") # or Date.today + 30
+
+          new_listing.save
+
+          unless new_listing.save
+            next
+          end
+        end
+      end
+      ellapsed = Time.now - start
+      sleep 1 if ellapsed < 1
+
+    end
+
+    #CreateHubspotDealJob.perform_later
+  end
+end
