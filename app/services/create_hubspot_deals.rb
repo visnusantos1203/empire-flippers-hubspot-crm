4251903b9fca4d8002ea5b566afac5a1a0ff1699
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-20 23:24:40 +0800

    Creates a service object for creating hubspot deals via Hubpost API

---
+++ app/services/create_hubspot_deals.rb
@@ -0,0 +1,29 @@
+class CreateHubspotDeals < ApplicationService
+  def self.run
+    to_upload = Listing.where(uploaded: false)
+
+    to_upload.each do |listing|
+      params = {
+                  "properties": {
+                    "amount": "#{listing.listing_price}",
+                    # Close_date should follow specific date format. Strftime converts datetime values to specified format.
+                    "closedate": "#{listing.close_date.strftime("%FT%TZ")}",
+                    "dealname": "Listing #{listing.listing_number}",
+                    "description": "#{listing.summary}"
+                  }
+                }
+      begin
+        Hubspot::Client.create_deal(params)
+      rescue
+        next
+      end
+      
+      listing.uploaded = true
+      listing.save
+
+      unless listing.save
+        next
+      end
+    end
+  end
+end
